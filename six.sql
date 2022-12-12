609d574cc6e49b8ae8ef7ed4b65f7b53179b020d
Kancheti Murali Krishna <70935879+murali-pixel@users.noreply.github.com>
2022-12-11 20:57:41 -0800

    Add files via upload

--- six.sql
+++ six.sql
@@ -7,9 +7,9 @@ USE Assignment3;
  Salary AS 'EE Salary',  
         
  CASE         	
-	WHEN Relationship = 'Spouse'  THEN Salary          
+	WHEN Relationship = 'Spouse'  THEN 0.5*Salary          
     END AS 'EE Death Benefit',     
 CASE         		
-    WHEN Relationship = 'Son'  THEN Salary   
+    WHEN Relationship = 'Son'  THEN 0.3*Salary   
     END AS 'Son Death Benefit',       
     Relationship AS Rel FROM employee, dependent WHERE employee.Ssn = dependent.Essn AND Relationship <> 'Daughter'

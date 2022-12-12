f2b9d1812dadbc4a1b7951f09a8d77229339ef55
Kancheti Murali Krishna <70935879+murali-pixel@users.noreply.github.com>
2022-12-11 21:07:54 -0800

    updated where statement

--- Nine.sql
+++ Nine.sql
@@ -1,2 +1,2 @@
 -- For each record in the Works_on table that shows hours greater than 15, display ESSN, PNO, Pname, Hours, Dnumber, Dname, Lname. Order by Lname then Hours. 
-select ESSN, PNO,Pname, Hours,Dnumber,Dname,Lname from Works_on JOIN Project,DEPARTMENT,EMPLOYEE WHERE Hours>15 order by Lname,Hours;
+select ESSN, PNO,Pname, Hours,Dnumber,Dname,Lname from Works_on JOIN Project,DEPARTMENT,EMPLOYEE WHERE Hours>15 AND Employee.SSN= Works_on.ESSN order by Lname,Hours;
